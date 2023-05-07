27770338c71181bc4ec47a89ccfd3381cf9f28d6
Bryan Arius <98681192+Bryanarius@users.noreply.github.com>
2023-05-07 19:56:06 -0400

    Added Password Hashing

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,7 +19,7 @@ const login = async (req, res) => {
     res.send('login user')
 }
 const updateUser = async (req, res) => {
-    res.send('updateUser user')
+    res.send(' updateUser ')
 }
 
 export {register, login, updateUser}
