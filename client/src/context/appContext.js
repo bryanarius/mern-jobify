7b8c9ffa20b5080bd0dd0e2ddeb87b6f3f3a9a97
Bryan Arius <98681192+Bryanarius@users.noreply.github.com>
2023-05-10 16:18:01 -0400

    Logout Functionality And Navbar

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -4,16 +4,12 @@ import reducer from './reducer'
 import axios from 'axios'
 import { 
     DISPLAY_ALERT, 
-    CLEAR_ALERT,
-    REGISTER_USER_BEGIN,
-    REGISTER_USER_ERROR,
-    REGISTER_USER_SUCCESS,
-    LOGIN_USER_BEGIN,
-    LOGIN_USER_ERROR,
-    LOGIN_USER_SUCCESS, 
+    CLEAR_ALERT, 
     SETUP_USER_BEGIN,
     SETUP_USER_ERROR,
-    SETUP_USER_SUCCESS 
+    SETUP_USER_SUCCESS,
+    TOGGLE_SIDEBAR,
+    LOGOUT_USER
 } from './actions'
 
 const token = localStorage.getItem('token')
@@ -28,7 +24,8 @@ const initialState = {
     user: user? JSON.parse(user) : null,
     token: null,
     userLocation: userLocation || '',
-    jobLocation: userLocation || ''
+    jobLocation: userLocation || '',
+    showSidebar: false,
 }
 
 const AppContext = React.createContext()
@@ -52,54 +49,11 @@ const addUserToLocalStorage = ({user,token,location}) =>{
     localStorage.setItem('token', token)
     localStorage.setItem('location', location)
 }
-const removeUserToLocalStorage = ({user,token,location}) =>{
+const removeUserToLocalStorage =()  =>{
     localStorage.removeItem('token')
     localStorage.removeItem('user')
     localStorage.removeItem('location')
 }
-
-
-const registerUser = async (currentUser) => {
-    dispatch({ type: REGISTER_USER_BEGIN})
-    try {
-        const response = await axios.post('/api/v1/auth/register', currentUser)
-        // console.log(response);
-        const {user, token, location} = response.data
-        dispatch(
-            {type: REGISTER_USER_SUCCESS,
-            payload: { user, token, location},
-        })
-        addUserToLocalStorage({user, token, location })
-    } catch (error) {
-        // console.log(error.response)
-        dispatch({
-            type: REGISTER_USER_ERROR,
-            payload: { msg: error.response.data.msg },
-        })
-    }
-    clearAlert()
-}
-
-    const loginUser = async (currentUser) => {
-        dispatch({ type: LOGIN_USER_BEGIN})
-    try {
-        const {data} = await axios.post('/api/v1/auth/login', currentUser)
-        // console.log(response);
-        const {user, token, location} = data
-        dispatch(
-            {type: LOGIN_USER_SUCCESS,
-            payload: { user, token, location},
-        })
-        addUserToLocalStorage({ user, token, location })
-    } catch (error) {
-        // console.log(error.response)
-        dispatch({
-            type: LOGIN_USER_ERROR,
-            payload: { msg: error.response.data.msg },
-        })
-    }
-    clearAlert()
-    }
 
     const setupUser = async ({currentUser, endPoint, alertText}) => {
         dispatch({ type: SETUP_USER_BEGIN})
@@ -122,8 +76,19 @@ const registerUser = async (currentUser) => {
     clearAlert()
     }
 
+    const logoutUser = () => {
+        dispatch({ type: LOGOUT_USER})
+        removeUserToLocalStorage()
+    }
+
+    const toggleSidebar = () =>{
+        dispatch({ type: TOGGLE_SIDEBAR })
+    }
     return (
-        <AppContext.Provider value={{...state, displayAlert, registerUser, loginUser, setupUser }}>{children}</AppContext.Provider>
+        <AppContext.Provider 
+        value={{...state, displayAlert, setupUser, toggleSidebar, logoutUser}}>
+            {children}
+        </AppContext.Provider>
         )
 }
 
--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -1,16 +1,13 @@
 import { 
 DISPLAY_ALERT, 
 CLEAR_ALERT, 
-REGISTER_USER_BEGIN, 
-REGISTER_USER_ERROR, 
-REGISTER_USER_SUCCESS,
-LOGIN_USER_BEGIN,
-LOGIN_USER_SUCCESS,
-LOGIN_USER_ERROR,
 SETUP_USER_BEGIN,
 SETUP_USER_SUCCESS,
 SETUP_USER_ERROR,
+TOGGLE_SIDEBAR,
+LOGOUT_USER
  } from "./actions"
+ import { initialState } from "./appContext"
 
 const reducer = (state,action) => {
     if (action.type === DISPLAY_ALERT){
@@ -30,53 +27,7 @@ const reducer = (state,action) => {
             alertText: '',
         }
     }
-    if(action.type === REGISTER_USER_BEGIN) {
-        return { ...state, isLoading: true}
-    }
-    if(action.type === REGISTER_USER_SUCCESS) {
-        return { 
-            ...state,
-             isLoading: false,
-             token:action.payload,
-             user:action.payload.user, userLocation:action.payload.location ,
-             showAlert:true,
-             alertType: 'success',
-             alertText: 'User Created Redirecting...'
-            }
-    }
-    if(action.type === REGISTER_USER_ERROR) {
-        return { 
-            ...state,
-             isLoading: false,
-             showAlert:true,
-             alertType: 'danger',
-             alertText: action.payload.msg,
-            }
-    }
 
-    if(action.type === LOGIN_USER_BEGIN) {
-        return { ...state, isLoading: true}
-    }
-    if(action.type === LOGIN_USER_SUCCESS) {
-        return { 
-            ...state,
-             isLoading: false,
-             token:action.payload,
-             user:action.payload.user, userLocation:action.payload.location ,
-             showAlert:true,
-             alertType: 'success',
-             alertText: 'Login Successful! Redirecting...'
-            }
-    }
-    if(action.type === LOGIN_USER_ERROR) {
-        return { 
-            ...state,
-             isLoading: false,
-             showAlert:true,
-             alertType: 'danger',
-             alertText: action.payload.msg,
-            }
-    }
     if(action.type === SETUP_USER_BEGIN) {
         return { ...state, isLoading: true}
     }
@@ -99,6 +50,23 @@ const reducer = (state,action) => {
              alertType: 'danger',
              alertText: action.payload.msg,
             }
+    }
+
+    if(action.type === TOGGLE_SIDEBAR) {
+        return {
+             ...state, 
+             showSidebar: !state.showSidebar,
+        }
+    }
+
+    if(action.type === LOGOUT_USER) {
+        return {
+            ...initialState,
+            user:null,
+            token:null,
+            jobLocation:'',
+            userLocation:'',
+        }
     }
         throw new Error(`no such action : ${action.type}`)
 }
