21186aef3e9bcfd6c2a4ea9953eb4d8cb848753c
Bryan Arius <98681192+Bryanarius@users.noreply.github.com>
2023-05-10 15:05:37 -0400

    Shared Layout

--- client/src/pages/dashboard/SharedLayout.js
+++ client/src/pages/dashboard/SharedLayout.js
@@ -1,15 +1,20 @@
 import { Outlet, Link} from 'react-router-dom'
 import Wrapper from '../../assets/wrappers/SharedLayout'
-import React from 'react'
+import {Navbar, BigSidebar, SmallSidebar} from '../../components'
 
 const SharedLayout = () => {
   return (
     <Wrapper>
-      <nav>
-        <Link to='add-job'>add job</Link>
-        <Link to='all-job'>all jobs</Link>
-      </nav>
-      <Outlet />
+      <main className="dashboard">
+        <SmallSidebar />
+        <BigSidebar />
+        <div>
+          <Navbar/>
+          <div className='dashboard-page'>
+            <Outlet />
+          </div>
+        </div>
+      </main>      
     </Wrapper>
     
   )
